88c63f309fb0a32f3d571a29d3d603be0f5df098
Naveenblip <84712340+Naveenblip@users.noreply.github.com>
2021-05-31 11:07:52 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -3,17 +3,18 @@
 #include <stdio.h>
 #include <math.h>
 
-float wallis_pi(int c)
+float wallis_pi(int it)
 {
   float pitotal=1;
   float term=0;
-    for(int i=1;i<=c;i++)
+    for(int i=1;i<=it;i++)
     {
         term=(4*pow(i,2));
         pitotal*=(term/(term-1));
     }
     return 2*pitotal;
 }
+
 int main(void) {
   float pi;
   for (int i=0; i<5; i++) {
